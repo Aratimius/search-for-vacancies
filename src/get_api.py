ac30dd65bb46c578700ab042f8af4ccf40bdee52
Aratimius <aratimius@gmail.com>
2023-11-02 17:41:03 +0300

    release 3.0: Добавлен класс Vacancy

--- src/get_api.py
+++ src/get_api.py
@@ -1,8 +1,7 @@
-import json
 from abc import ABC, abstractmethod
 import requests
 from datetime import datetime
-from .functions import get_id  # точка означает, что я к этой функции буду обращаться в main, что лежит на каталог выше
+from src.functions import get_id  # точка означает, что я к этой функции буду обращаться в main, что лежит на каталог выше
 
 
 class API(ABC):
@@ -39,13 +38,14 @@ class HeadHunter(API):
             # Не у всех вакансий есть url, а мне хотелось бы отслеживать url:
             if 'url' in data.keys():
                 new_data['url'] = data['url']
-            return_data.append(new_data)
             # Та же история с зарплатой, там вообще мрак:
             if 'salary' in data.keys() and isinstance(data['salary'], dict)\
                     and data['salary'] is not None and data['salary']['from'] is not None:
                 new_data['payment_from'] = data['salary']['from']
             else:
                 new_data['payment_from'] = 0
+            vacancy = Vacancy(new_data)
+            return_data.append(vacancy.get_dict())
         return return_data
 
 
@@ -82,8 +82,29 @@ class SuperJob(API):
                         'published_date': data['date_published'], 'payment_from': data['payment_from']}
             if 'url' in data['client'].keys():
                 new_data['url'] = data['client']['url']
-            return_data.append(new_data)
+            vacancy = Vacancy(new_data)
+            return_data.append(vacancy.get_dict())
             count -= 1
             if count == 0:
                 break
         return return_data
+
+
+class Vacancy:
+    """Класс для работы с вакансиями"""
+    def __init__(self, vacancy_data):
+        self.profession = vacancy_data['profession']
+        self.city = vacancy_data['city']
+        self.date = vacancy_data['published_date']
+        self.url = vacancy_data['url']
+        self.payment = vacancy_data['payment_from']
+
+    def __str__(self):
+        return (f"'profession': {self.profession}, 'city': {self.city},"
+                f" 'date': {self.date}, 'payment_from': {self.payment}")
+
+    def __le__(self, other):
+        return self.payment <= other.payment
+
+    def get_dict(self):
+        return {'profession': self.profession, 'city': self.city, 'published_date': self.date, 'payment_from': self.payment}
